12bce61f0a091a5e46c75a6335d5a9e4d99b64b2
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-01-31 23:19:53 +0100

    one more test

--- test/test.jl
+++ test/test.jl
@@ -3,3 +3,11 @@
 
 # GridLayout constructor
 @timed GridLayout()
+
+# Big GridLayout
+@timed let
+    gl = GridLayout()
+    for i in 1:10, j in 1:10, k in 1:10
+        gl[i, j] = GridLayout()
+    end
+end
