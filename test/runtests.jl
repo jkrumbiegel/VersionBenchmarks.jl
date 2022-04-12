043018d19b9b22a418c231038b09d6b3b72cc60e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-04-12 17:10:28 +0200

    remove stale tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,2 @@
 using VersionBenchmarks
 using Test
-
-@testset "VersionBenchmarks.jl" begin
-    df = VersionBenchmarks.benchmark(
-        joinpath(@__DIR__, "TestRepo"),
-        [joinpath(@__DIR__, "test.jl")],
-        ["master", "optimizations"],
-        repetitions = 10
-    )
-end
